7ea2ca44d7a3bc58048a3fde2ea3a9e694de2254
Jay Bosamiya <jaybosamiya@gmail.com>
2017-08-07 04:21:12 +0530

    Fix indentation

--- IAAI.c
+++ IAAI.c
@@ -3,20 +3,20 @@
 #include <signal.h>
 
 int main() {
-	system("/bin/stty raw");
+  system("/bin/stty raw");
 
-	int i = 0;
-	char str[] = "I AM AN IDIOT ";
-	char c = 'a';
-	while(1){
-		for(i = 0; i<14; i++) {
-			c = getchar();
-			printf("\b%c", str[i]);
-		}
-		system("/bin/stty cooked");
-		printf("\n");
-		system("/bin/stty raw");
-	}
+  int i = 0;
+  char str[] = "I AM AN IDIOT ";
+  char c = 'a';
+  while(1){
+    for(i = 0; i<14; i++) {
+      c = getchar();
+      printf("\b%c", str[i]);
+    }
+    system("/bin/stty cooked");
+    printf("\n");
+    system("/bin/stty raw");
+  }
 	
-	return 0;
+  return 0;
 }
